5d809e8062c5333839d6aaeff13d9953b47d2d83
George <51861795+gloulakis@users.noreply.github.com>
2022-02-24 22:25:31 +0200

    Initial Commit

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Georgios Loulakis on 24.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
